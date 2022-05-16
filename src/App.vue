de0928fda471f97c7e3c76466f0f144f2c1107bb
bayabdi <35993115+bayabdi@users.noreply.github.com>
2022-05-16 11:22:47 +0600

    Init

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,6 @@ export default {
   metaInfo: {
     title: 'INDEX'
   },
-  meta: [
-    { charset: 'utf-8' },
-    { name: 'description', content: 'bot' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' }
-  ],
   data: () => ({
     //
   }),
